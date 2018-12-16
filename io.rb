2b19d667dbe8a6d12e78a7deb85589834219f5fe
Simon Tharby <jinjamail@gmail.com>
2018-12-16 16:41:00 +0000

    create separate mk_dir def

--- io.rb
+++ io.rb
@@ -5,20 +5,27 @@ module Io
     t.min < 10 ? min = '0' + t.min.to_s : min = t.min
     t.sec < 10 ? sec = '0' + t.sec.to_s : sec = t.sec
 
-    if incomplete == false
+    if incomplete == true
       filename = "incomplete_#{t.year}-#{t.month}-#{t.day}_#{hour}h#{min}m#{sec}s"
     else
       filename = "complete_#{t.year}-#{t.month}-#{t.day}_#{hour}h#{min}m#{sec}s"
     end
   end
 
-
-  def self.autosave
+  def self.mk_dir(incomplete = true)
     dirname = "games"
     Dir.mkdir(dirname) unless File.exists?dirname
-    dirname = "games/complete"
-    Dir.mkdir(dirname) unless File.exists?dirname
+    if incomplete == true
+      dirname = "games/incomplete"
+      Dir.mkdir(dirname) unless File.exists?dirname
+    else
+      dirname = "games/complete"
+      Dir.mkdir(dirname) unless File.exists?dirname
+    end
+  end
 
+  def self.autosave
+    mk_dir(incomplete = true)
     filename = create_filename(incomplete = true)
     puts filename
   end
