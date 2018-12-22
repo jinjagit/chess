762242a1df8bada194c5e7b24f258eba5c490602
Simon Tharby <jinjamail@gmail.com>
2018-12-22 15:50:15 +0000

    split pgn file load data into info & moves

--- pgn_read.rb
+++ pgn_read.rb
@@ -1,23 +1,49 @@
 require './io'
 
-def parse_pgn(filename)
+def parse_pgn(data)
   error = 'none'
+  event = nil
+  date = nil
+  white = nil
+  black = nil
+  result = nil
+  info = nil
+  pgn_list = ['Nf3', 'e5']
 
   begin
-    puts
-    puts "loading: #{filename}"
-    data = Io.load_file(filename)
-    puts "data from #{filename}:"
+    print "data in: "
     p data
+    puts
+    temp = data.strip.split("\n")
+    info = temp[0..-2]
+    pgn_list = temp[-1]
   rescue StandardError => error
-    puts "ERROR! #{error}" unless error == 'none'
   end
+
+  unless error == 'none'
+    puts "ERROR! #{error}" unless error == 'none' # open error window & print
+  else # parse data components and then test moves for legality
+    print "Event: #{event}  "
+    print "Date: #{date}  "
+    print "White: #{white}  "
+    print "Black: #{black}  "
+    puts "Result: #{result}  "
+    puts "info: #{info}"
+    puts "Moves: #{pgn_list}"
+    puts "---------------------------------------------------------------------------------------------------------"
+  end
+
 
 end
 
+system "clear"
+files = ['test1.pgn', 'uppercase_ext.PGN', 'lichess_pgn_2018.12.21_Human_vs_Human.ozTcKlq3.pgn']
 
-files = ['test1.pgn', 'does_not_exist.pgn', 'uppercase_ext.PGN']
+files.each do |filename|
+  puts "loading: #{filename}"
+  data = Io.load_file(filename)
+  parse_pgn(data)
+end
 
-files.each {|filename| parse_pgn(filename)}
 puts
-puts "I am still running after error :-)"
+puts "I am still running, even after error :-)"
