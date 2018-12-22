ccb210c80ed21166cae9a41d2d0897e7f5374242
Simon Tharby <jinjamail@gmail.com>
2018-12-22 13:38:38 +0000

    basic error handling on load file in parse_pgn (using non-existant file for test)

--- pgn_read.rb
+++ pgn_read.rb
@@ -1,4 +1,23 @@
 require './io'
 
-data = Io.load_file('test1.pgn')
-p data
+def parse_pgn(filename)
+  error = 'none'
+
+  begin
+    puts "loading: #{filename}"
+    data = Io.load_file(filename)
+    puts "data from #{filename}:"
+    p data
+    puts
+  rescue StandardError => error
+    puts "ERROR! #{error}" unless error == 'none'
+  end
+
+end
+
+
+files = ['test1.pgn', 'does_not_exist.pgn']
+
+files.each {|filename| parse_pgn(filename)}
+puts
+puts "I am still running after error :-)"
