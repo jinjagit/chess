d859b7dd352a69a35aa9b671d9dbb179df8e1a39
Simon Tharby <jinjamail@gmail.com>
2018-12-24 12:02:24 +0000

    parse info for title & moves for pgn_list from complete game load data

--- pgn_read.rb
+++ pgn_read.rb
@@ -1,49 +1,64 @@
 require './io'
 
-def parse_pgn(data)
+def print_parsed(filename, list, info, pgn_list)
+  if info == nil || list.all? {|k, v| v == nil}
+    title = "File: #{filename} (no details)"
+  else
+    title = "#{list['[white ']} v #{list['[black ']} (#{list['[event ']}, #{list['[date ']})"
+  end
+  # shorten overly long title(s)
+  puts "Title: #{title}"
+  puts "Moves: #{pgn_list}"
+  puts "---------------------------------------------------------------------------------------------------------"
+end
+
+def parse_pgn(data, filename)
   error = 'none'
-  event = nil
-  date = nil
-  white = nil
-  black = nil
-  result = nil
+  list = {'[event ' => nil, '[date ' => nil, '[white ' => nil, '[black ' => nil}
   info = nil
-  pgn_list = ['Nf3', 'e5']
+  title = ''
+  pgn_list = ''
 
   begin
-    print "data in: "
-    p data
-    puts
-    temp = data.strip.split("\n")
-    info = temp[0..-2]
-    pgn_list = temp[-1]
+    if data.include?(']')
+      temp = data.strip.split(']')
+      info = temp[0..-2].join.strip.split("\n")
+      pgn_list = temp[-1].strip.split("\n").join(' ')
+      list.each_key do |key|
+        info.each do |e|
+          if e.downcase.include?(key)
+            list[key] = e.split("\"")[1]
+          end
+        end
+      end
+    else
+      pgn_list = data.strip.split("\n").join
+    end
+
+    pgn_list = pgn_list.split('}')
+    pgn_list.map! {|e| e.split(' {')[0]}
+    pgn_list = pgn_list.join.strip.split(" ").delete_if {|e| e.include?('.')}
   rescue StandardError => error
+    error = 'Cannot parse file contents'
   end
 
   unless error == 'none'
     puts "ERROR! #{error}" unless error == 'none' # open error window & print
   else # parse data components and then test moves for legality
-    print "Event: #{event}  "
-    print "Date: #{date}  "
-    print "White: #{white}  "
-    print "Black: #{black}  "
-    puts "Result: #{result}  "
-    puts "info: #{info}"
-    puts "Moves: #{pgn_list}"
-    puts "---------------------------------------------------------------------------------------------------------"
+    print_parsed(filename, list, info, pgn_list)
   end
-
-
 end
 
+
+
 system "clear"
-files = ['test1.pgn', 'uppercase_ext.PGN', 'lichess_pgn_2018.12.21_Human_vs_Human.ozTcKlq3.pgn']
+files = ['test1.pgn', 'uppercase_ext.PGN', 'no_info.pgn',
+        'lichess_pgn_2018.12.21_Human_vs_Human.ozTcKlq3.pgn',
+        'spassky_fischer_1972.pgn']
 
 files.each do |filename|
   puts "loading: #{filename}"
+  puts
   data = Io.load_file(filename)
-  parse_pgn(data)
+  parse_pgn(data, filename)
 end
-
-puts
-puts "I am still running, even after error :-)"
