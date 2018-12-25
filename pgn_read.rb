d391b8793e1ab12e6e2cb33f635dbdb3c4128cc5
Simon Tharby <jinjamail@gmail.com>
2018-12-25 14:33:36 +0000

    pgn_read.rb rejects too few or too many chars in move

--- pgn_read.rb
+++ pgn_read.rb
@@ -37,12 +37,17 @@ def parse_pgn(data, filename)
 
     pgn_list = pgn_list.split('}').map! {|e| e.split(' {')[0]}
     pgn_list = pgn_list.join.strip.split(" ").delete_if {|e| e.include?('.')}
+    pgn_list.each do |e|
+      error = "bad move format found" if e.length < 2 || e.length > 7
+    end
+
   rescue StandardError => error
     error = 'cannot parse file contents'
   end
 
   unless error == 'none'
     puts "ERROR! #{error}" # print error to load_save menu && do not load game
+    puts "---------------------------------------------------------------------------------------------------------"
   else # parse data components
     print_parsed(filename, list, info, pgn_list)
     # to do:
@@ -57,7 +62,8 @@ end
 system "clear"
 files = ['test1.pgn', 'uppercase_ext.PGN', 'no_info.pgn',
         'lichess_pgn_2018.12.21_Human_vs_Human.ozTcKlq3.pgn',
-        'spassky_fischer_1972.pgn', 'extra_spaces.pgn']
+        'spassky_fischer_1972.pgn', 'extra_spaces.pgn', 'move_too_short.pgn',
+        'move_too_long.pgn']
 
 files.each do |filename|
   puts "loading: #{filename}"
